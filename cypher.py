e6a19f23f0cc073a49bd4c44d4de26f3aa5004f3
Barra Darcy <baradarcy@gmail.com>
2019-12-30 17:16:14 +0000

    Add files via upload
    This is a program that write coded messages. You input a 26Char string (it could be a sentence) and it uses it to code your message. The message is saved in a text file (which you name) that you can do whatever with and can be deciphered again by anybody with the pass string....or a very clever person:-) It's a bit of fun. If you wanna edit it and improve it feel free but let me know how you did that and point me to the new code:-) Cheers!

---
+++ cypher.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+print ("{:>60}".format("barradarcy@hotmail.com"))
+print ("{:>60}".format(" "))
+npt = raw_input (("26 Char Cyper Sentence: ").lower())
+L = list(npt)
+r = raw_input ('{name of file}: ')
+txt='.txt'
+me=r+txt
+folder = open(me, 'a')
+while True:
+ a = L[0];b = L[1];c = L[2];d = L[3];e = L[4];f = L[5];g = L[6];h = L[7];
+ i = L[8];j = L[9];k = L[10];l = L[11];m = L[12];n = L[13];o = L[14];p = L[15];
+ q = L[16];r = L[17];s = L[18];t = L[19];u = L[20];v = L[21];w = L[22];x = L[23];y = L[24];z = L[25]
+ txtt = raw_input (('{\n').lower())
+ txt = txtt.lower ()
+ new1 = txt.replace ('a', a)
+ new2 = new1.replace ('b', b)
+ new3 = new2.replace ('c', c)
+ new4 = new3.replace ('d', d)
+ new5 = new4.replace ('e', e)
+ new6 = new5.replace ('f', f)
+ new7 = new6.replace ('g', g)
+ new8 = new7.replace ('h', h)
+ new9 = new8.replace ('i', i)
+ new10 = new9.replace ('j', j)
+ new11 = new10.replace ('k', k)
+ new12 = new11.replace ('l', l)
+ new13 = new12.replace ('m', m)
+ new14 = new13.replace ('n', n)
+ new15 = new14.replace ('o', o)
+ new16 = new15.replace ('p', p)
+ new17 = new16.replace ('q', q)
+ new18 = new17.replace ('r', r)
+ new19 = new18.replace ('s', s)
+ new20 = new19.replace ('t', t)
+ new21 = new20.replace ('u', u)
+ new22 = new21.replace ('v', v)
+ new23 = new22.replace ('w', w)
+ new24 = new23.replace ('x', x)
+ new25 = new24.replace ('y', y)
+ new26 = new25.replace ('z', z)
+ print (new26.lower ())
+ folder.write ((new26).lower ())
